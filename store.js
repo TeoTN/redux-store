d6196f6497f494e99de918926adb8371733439d0
Piotr Staniów <staniowp@gmail.com>
2018-02-19 16:06:03 +0100

    feat(Store): Implement the store

--- store.js
+++ store.js
@@ -1,3 +1,30 @@
-export class Store {
+class Store {
+    constructor(initialState = {}, reducers) {
+        this.state = initialState;
+        this.reducers = reducers;
+        this.listeners = [];
+    }
 
+    dispatch(action) {
+        let nextState = {};
+        for (const scope in this.reducers) {
+            const reducer = this.reducers[scope];
+            nextState[scope] = reducer(this.state[scope], action);
+        }
+        this.state = nextState;
+        this.notify(nextState);
+    }
+
+    notify(nextState) {
+        this.listeners.forEach(listener => listener(nextState));
+    }
+
+    subscribe(listener) {
+        const index = this.listeners.push(listener);
+        return () => {
+            this.listeners.splice(index - 1, 1);
+        };
+    }
 }
+
+module.exports = Store;
